b60ff5ba82743200d69688586b95cbbc780635b8
TheRedGrim07 <ayushsinha7225@gmail.com>
2025-12-28 17:59:09 +0530

    Final commit of the day

--- Api_fetcher.py
+++ Api_fetcher.py
@@ -1,25 +1,35 @@
 import requests
 import csv
-
-url=r"https://jsonplaceholder.typicode.com/users"
-response=requests.get(url)
-
-if response.status_code==200:
-    print('File Fetched')
-    users=response.json()
-else:
-    print('Error')
+from os import path
 
 
-with open("Users.csv",mode="w",newline="") as file:
-    writer=csv.writer(file)
-    writer.writerow(['Name','Email','City'])
+def fetch_api_data(url,location):
+    response=requests.get(url)
 
-    for user in users:
-        name=user.get('name',"N/A")
-        email=user.get('email',"N/A")
-        city=user.get("address",'{}').get("city","N/A")
+    if response.status_code==200:
+        print('File Fetched')
+        users=response.json()
+    else:
+        print('Error')
 
-        writer.writerow([name," || ",email," || ",city])
 
-print("Success")
+    nw_location=path.join(location,"Users.csv")
+    with open(nw_location,mode="w",newline="") as file:
+        writer=csv.writer(file)
+        writer.writerow(['Name','Email','City'])
+
+        for user in users:
+            name=user.get('name',"N/A")
+            email=user.get('email',"N/A")
+            city=user.get("address",'{}').get("city","N/A")
+
+            writer.writerow([name," || ",email," || ",city])
+
+    print("Success")
+
+
+url=input("Enter URL: ")
+location=input("Enter Location to save file: ")
+
+fetch_api_data(url,location)
+
