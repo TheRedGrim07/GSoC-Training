af694c957b6522c4c3424ac6ee7c42a47205a566
TheRedGrim07 <ayushsinha7225@gmail.com>
2025-12-29 21:30:38 +0530

    new edit

--- Api_fetcher/Api_fetcher_argpharse.py
+++ Api_fetcher/Api_fetcher_argpharse.py
@@ -5,17 +5,21 @@ import argparse
 
 class ApiFetcher:
 
-    def __init__(self, url):
+    def __init__(self, url,quiet=False,city=None):
         self.url=url
         self.users=[]
+        self.quiet=quiet
+        self.city=city
     
     def fetch_data(self):
         response=requests.get(self.url)
         if response.status_code==200:
-            print("Data fetched successfully")
+            if not self.quiet:
+                print("Data fetched successfully")
             self.users=response.json()
         else:
-            print("Failed to fetch data. Status code:", response.status_code)
+            if not self.quiet:
+                print("Failed to fetch data. Status code:", response.status_code)
     
     def save_to_csv(self,filename,location):
         filename=path.join(location, filename)
@@ -27,12 +31,20 @@ class ApiFetcher:
                 email=user.get('email','n/a')
                 city=user.get('address',{}).get('city','n/a')
                 lat=user.get('address',{}).get('geo',{}).get('lat','0.000000')
-                writer.writerow([name,email,city,lat])
+                if self.city:
+                    if city.lower()==self.city.lower():
+                        writer.writerow([name,email,city,lat])
+                else:
+                    continue
+
                 
+                
+        if not self.quiet:
+            print(f"Data saved to {filename} successfully.")
         
-        print(f"Data saved to {filename} successfully.")
 
     def run(self, filename,location):
+
         self.fetch_data()
         self.save_to_csv(filename,location)
 
@@ -42,9 +54,14 @@ if __name__=="__main__":
 
     parse=argparse.ArgumentParser(description="Fetch user data from API and save to CSV")
     parse.add_argument('--output', type=str,default="users_data.csv", help="Name of the output CSV file")
+    parse.add_argument('--quiet', action='store_true', help="Suppress output messages")
+    parse.add_argument('--city', type=str, help="Mention the city to filter users")
+
+
+
     args=parse.parse_args()
     output=args.output
 
     url="https://jsonplaceholder.typicode.com/users"
-    fetcher=ApiFetcher(url)
+    fetcher=ApiFetcher(url,args.quiet,args.city)
     fetcher.run(output,r'.')
